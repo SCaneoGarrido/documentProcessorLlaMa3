1a7ce0a05fadedcf04a195ad538ca56a80b0b516
Sebastian Caneo <sebamc403@gmail.com>
2024-07-08 23:03:35 -0400

    algo

--- App/app.py
+++ App/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify #type: ignore
+from flask import Flask, render_template, jsonify, request #type: ignore
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
@@ -9,5 +9,22 @@ def home():
   return render_template("index.html")
 
 
+
+@app.route('/processing', methods=['POST'])
+def processing():
+  if request.method == 'POST':
+    if request.method == 'POST':
+      
+      if 'file' not in request.files:
+        return "No file part", 400
+      
+      file = request.files['file']
+      if file.filename == '':
+        return jsonify({'Error': 'No selected file'}), 400
+      
+      return jsonify({'success':f"Archivo recibido {file.filename}"}), 200
+
+    else:
+      return "Method not allowed", 405              
 if __name__ == "__main__":
   app.run(debug=True)
---
+++ App/modules/llama3_controler.py
@@ -0,0 +1,7 @@
+# Este modulo es para crear la logica de los controles para llama3
+
+class Llama3Controler:
+  def __init__(self) -> None:
+    pass
+
+  
